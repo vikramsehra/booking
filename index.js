eb68483cfc49b8ddb95045edef437b16ecea51b0
Vikram Kumar Meena <vikramkr.meena.min16@itbhu.ac.in>
2023-03-19 00:10:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+import cookieParser from 'cookie-parser'
+import cors from "cors"
+const app = express();
+dotenv.config()
+
+const path = require('path')
+
+const PORT = process.env.PORT || 8800
+
+mongoose.set("strictQuery", false);
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB.")
+    } catch (error) {
+        throw error
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!")
+})
+
+// middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+// static files
+app.use(express.static(path.join(__dirname, './bookingclient/dist')));
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./bookingclient/dist/index.html"))
+})
+
+
+// error handler middleware
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong!"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    })
+
+})
+
+app.listen(PORT, () => {
+    connect();
+    console.log("Connected to backend.");
+})
