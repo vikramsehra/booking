b0e33cd98e3cbfd8b05d3d9b330f5ecfaf9042c8
Vikram Kumar Meena <vikramkr.meena.min16@itbhu.ac.in>
2023-03-19 21:14:47 +0530

    final commit

--- index.js
+++ index.js
@@ -39,17 +39,16 @@ app.use(cookieParser())
 app.use(express.json())
 
 
-// static files
-app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)));
-app.use("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "dist", "index.html"))
-})
-
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
+// static files
+app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)));
+app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"))
+})
 
 
 // error handler middleware
